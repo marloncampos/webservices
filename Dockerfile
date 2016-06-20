26d3516aac5db50907b5d7ac43edf166bebe8660
marloncampos <campos.marlon@gmail.com>
2016-06-20 01:59:48 -0300

    First update

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,9 @@ ADD . /work/src/github.com/marloncampos/webservices
 # Build the webservices command inside the container.
 # (You may fetch or manage dependencies here,
 # either manually or with a tool like "godep".)
+RUN apk add --no-cache git \
+	&& go get github.com/CiscoZeus/go-zeusclient \
+	&& apk del git
 RUN go install github.com/marloncampos/webservices
 
 # Run the web command by default when the container starts.
--- main.go
+++ main.go
@@ -1,15 +1,44 @@
-package main
+@@ -2,8 +2,11 @@ package main
 
 import (
     "fmt"
+	"github.com/CiscoZeus/go-zeusclient"
+	"io"
     "net/http"
+	"os"
+	"runtime"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "I do like %s very much!", r.URL.Path[1:])
+@@ -13,8 +16,33 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	// print env variables
+	fmt.Println("OS:", runtime.GOOS)
+	fmt.Println("USER:", os.Getenv("USER"))
+	fmt.Println("HOME:", os.Getenv("HOME"))
+	fmt.Println("HOSTNAME:", os.Getenv("HOSTNAME"))
+
+	// send env variables to zeus
+	z := &zeus.Zeus{ApiServ: "http://api.ciscozeus.io", Token: os.Getenv("ZEUS_TOKEN")}
+	logs := zeus.LogList{
+		Name: "syslog",
+		Logs: []zeus.Log{
+			zeus.Log{
+				"OS":       runtime.GOOS,
+				"USER":     os.Getenv("USER"),
+				"HOME":     os.Getenv("HOME"),
+				"HOSTNAME": os.Getenv("HOSTNAME")},
+		},
+	}
+	suc, err := z.PostLogs(logs)
+	if err != nil {
+		fmt.Println("Zeus response:", err.Error())
+	} else {
+		fmt.Println("Zeus response:", suc)
+	}
+ 	fmt.Printf("Starting http server...\n")
+ 	http.HandleFunc("/", handler)
+	//err2 := http.ListenAndServe(":8080", nil)
+	//fmt.Println(err2.Error())
 }
